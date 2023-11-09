df692a6bda494f30fc052f1a8a4ffd7a50ae8c0e
Rababmedle <rabab.elmamy@gmail.com>
2023-11-09 13:32:24 +0100

    add a new endpoint

--- 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/formation/springboot/myapp1/rest/FunrestController.java
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/formation/springboot/myapp1/rest/FunrestController.java
@@ -4,12 +4,22 @@ package com.formation.springboot.myapp1.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//expose "/" that return hello word
 @RestController
 public class FunrestController {
     @GetMapping("/")
     public String sayhello(){
         return "hello world";
     }
+//expose a new endpoint workout
+
+    @GetMapping("/workout")
+    public String getDetailworkout(){
+        return "Run a hard 5k";
+    }
+
+
 
 }
+//
+
