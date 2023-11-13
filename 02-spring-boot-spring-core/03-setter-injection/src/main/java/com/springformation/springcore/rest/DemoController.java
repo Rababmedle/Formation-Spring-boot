76df8dc3f2cd9d01e3000e15a8dd77acb4441f5b
Rababmedle <rabab.elmamy@gmail.com>
2023-11-13 12:51:02 +0100

    03-setter-injection

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/springformation/springcore/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.springformation.springcore.rest;
+
+import com.springformation.springcore.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private Coach myCoach;
+//    @Autowired
+//    public DemoController(Coach theCoach){
+//        myCoach=theCoach;
+//    }
+@Autowired
+public void  setcoach(Coach theCoach){
+    myCoach=theCoach;
+}
+    @GetMapping("/getdailyworkout")
+    public String getdailyworkout(){
+        return myCoach.getdailyworkout();
+    }
+}
